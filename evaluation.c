6152dc7270df9a0fee76b21bc9def7163b04c358
Lorenzo Gil Sanchez <lorenzo.gil.sanchez@gmail.com>
2015-11-16 04:30:50 +0000

    Evaluate the AST

--- evaluation.c
+++ evaluation.c
@@ -6,6 +6,38 @@
 
 #include "mpc.h"
 
+long eval_op (long x, char* op, long y) {
+  if (strcmp(op, "+") == 0) {
+    return x + y;
+  } else if (strcmp(op, "-") == 0) {
+    return x - y;
+  } else if (strcmp(op, "*") == 0) {
+    return x * y;
+  } else if (strcmp(op, "/") == 0) {
+    return x / y;
+  } else {
+    return 0;
+  }
+}
+
+long eval (mpc_ast_t* t) {
+
+  if (strstr(t->tag, "number")) {
+    return atoi(t->contents);
+  }
+
+  char* op = t->children[1]->contents;
+
+  long x = eval(t->children[2]);
+
+  int i = 3;
+  while (strstr(t->children[i]->tag, "expr")) {
+    x = eval_op(x, op, eval(t->children[i]));
+    i++;
+  }
+
+  return x;
+}
 
 int main (int argc, char** argv) {
 
@@ -24,7 +56,7 @@ int main (int argc, char** argv) {
       lispy: /^/ <operator> <expr>+ /$/ ;           \
     ", Number, Operator, Expr, Lispy);
 
-  puts("Lispy Version 0.0.0.0.2");
+  puts("Lispy Version 0.0.0.0.3");
   puts("Press Ctrl+c to Exit\n");
 
   while (1) {
@@ -35,7 +67,8 @@ int main (int argc, char** argv) {
 
     mpc_result_t r;
     if (mpc_parse ("<stdin>", input, Lispy, &r)) {
-      mpc_ast_print(r.output);
+      long result = eval(r.output);
+      printf("%li\n", result);
       mpc_ast_delete(r.output);
     } else {
       mpc_err_print(r.error);
