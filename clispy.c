120ce05d338e90d7014ab507569c9a915ab0960d
Lorenzo Gil Sanchez <lorenzo.gil.sanchez@gmail.com>
2015-11-30 05:29:47 +0000

    Allow user defined variables

--- clispy.c
+++ clispy.c
@@ -437,6 +437,31 @@ lval* builtin_div (lenv* e, lval* a) {
   return builtin_op(e, a, "/");
 }
 
+lval* builtin_def (lenv* e, lval* a) {
+  LASSERT(a, a->cell[0]->type == LVAL_QEXPR,
+	  "Function 'def' passed incorrect type!");
+
+  lval* syms = a->cell[0];
+
+  /* Ensure all elements of first list are symbols */
+  for (int i=0; i < syms->count; i++) {
+    LASSERT(a, syms->cell[i]->type == LVAL_SYM,
+	    "Function 'def' cannot define non-symbol");
+  }
+
+  /* Check correct number of symbols and values */
+  LASSERT(a, syms->count == a->count - 1,
+	  "Function 'def' cannot define incorrect number of values to symbols");
+
+  /* Assign copies of values to symbols */
+  for (int i=0; i < syms->count; i++) {
+    lenv_put(e, syms->cell[i], a->cell[i + 1]);
+  }
+
+  lval_del(a);
+  return lval_sexpr();
+}
+
 void lenv_add_builtins (lenv* e) {
   lenv_add_builtin(e, "list", builtin_list);
   lenv_add_builtin(e, "head", builtin_head);
@@ -448,6 +473,8 @@ void lenv_add_builtins (lenv* e) {
   lenv_add_builtin(e, "-", builtin_sub);
   lenv_add_builtin(e, "*", builtin_mul);
   lenv_add_builtin(e, "/", builtin_div);
+
+  lenv_add_builtin(e, "def", builtin_def);
 }
 
 lval* lval_eval_sexpr(lenv* e, lval* v) {
