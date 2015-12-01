7d16b79e904810ec75cccee10dc241e3f76e3180
Lorenzo Gil Sanchez <lorenzo.gil.sanchez@gmail.com>
2015-12-01 05:26:38 +0000

    Better error handling and fix a lval_copy bug

--- clispy.c
+++ clispy.c
@@ -6,8 +6,12 @@
 
 #include "mpc.h"
 
-#define LASSERT(args, cond, err) \
-  if (!(cond)) { lval_del(args); return lval_err(err); }
+#define LASSERT(args, cond, fmt, ...)				\
+  if (!(cond)) { \
+    lval* err = lval_err(fmt, ##__VA_ARGS__); \
+    lval_del(args); \
+    return err; \
+  }
 
 /* forward declarations */
 struct lval;
@@ -48,11 +52,21 @@ lval* lval_num (long x) {
   return v;
 }
 
-lval* lval_err (char* msg) {
+lval* lval_err (char* fmt, ...) {
   lval* v = malloc(sizeof(lval));
   v->type = LVAL_ERR;
-  v->err = malloc(strlen(msg) + 1);
-  strcpy(v->err, msg);
+
+  va_list va;
+  va_start(va, fmt);
+
+  v->err = malloc(512);
+
+  vsnprintf(v->err, 511, fmt, va);
+
+  v->err = realloc(v->err, strlen(v->err) + 1);
+
+  va_end(va);
+
   return v;
 }
 
@@ -150,6 +164,7 @@ lval* lval_copy (lval* v) {
   case LVAL_SEXPR:
   case LVAL_QEXPR:
     x->count = v->count;
+    x->cell = malloc(sizeof(lval*) * x->count);
     for (int i=0; i < v->count; i++) {
       x->cell[i] = lval_copy(v->cell[i]);
     }
@@ -293,7 +308,7 @@ lval* lenv_get (lenv* e, lval* k) {
       return lval_copy(e->vals[i]);
     }
   }
-  return lval_err("Unbound symbol!");
+  return lval_err("Unbound symbol '%s'!", k->sym);
 }
 
 void lenv_put (lenv* e, lval* k, lval* v) {
@@ -322,9 +337,26 @@ void lenv_add_builtin (lenv* e, char* name, lbuiltin func) {
   lval_del(v);
 }
 
+char* ltype_name(int t) {
+  switch(t) {
+  case LVAL_FUN: return "Function";
+  case LVAL_NUM: return "Number";
+  case LVAL_ERR: return "Error";
+  case LVAL_SYM: return "Symbol";
+  case LVAL_SEXPR: return "S-Expression";
+  case LVAL_QEXPR: return "Q-Expression";
+  default: return "Unknown";
+  }
+}
+
 lval* builtin_head(lenv* e, lval* a) {
-  LASSERT(a, a->count == 1, "Function 'head' passed too many arguments!");
-  LASSERT(a, a->cell[0]->type == LVAL_QEXPR, "Function 'head' passed incorrect type!");
+  LASSERT(a, a->count == 1,
+	  "Function 'head' passed too many arguments. "
+	  "Got %i, Expected %i.", a->count, 1);
+  LASSERT(a, a->cell[0]->type == LVAL_QEXPR,
+	  "Function 'head' passed incorrect type for argument 0. "
+	  "Got %s, Expected %s.",
+	  ltype_name(a->cell[0]->type), ltype_name(LVAL_QEXPR));
   LASSERT(a, a->cell[0]->count != 0, "Function 'head' passed {}!");
 
   lval* v = lval_take(a, 0);
@@ -337,8 +369,13 @@ lval* builtin_head(lenv* e, lval* a) {
 }
 
 lval* builtin_tail(lenv* e, lval* a) {
-  LASSERT(a, a->count == 1, "Function 'tail' passed too many arguments!");
-  LASSERT(a, a->cell[0]->type == LVAL_QEXPR, "Function 'tail' passed incorrect type!");
+  LASSERT(a, a->count == 1,
+	  "Function 'tail' passed too many arguments. "
+	  "Got %i, Expected %i.", a->count, 1);
+  LASSERT(a, a->cell[0]->type == LVAL_QEXPR,
+	  "Function 'tail' passed incorrect type for argument 0. "
+	  "Got %s, Expected %s.",
+	  ltype_name(a->cell[0]->type), ltype_name(LVAL_QEXPR));
   LASSERT(a, a->cell[0]->count != 0, "Function 'tail' passed {}!");
 
   lval* v = lval_take(a, 0);
@@ -354,8 +391,13 @@ lval* builtin_list(lenv* e, lval* a) {
 }
 
 lval* builtin_eval(lenv* e, lval* a) {
-  LASSERT(a, a->count == 1, "Function 'eval' passed too many arguments!");
-  LASSERT(a, a->cell[0]->type == LVAL_QEXPR, "Function 'eval' passed incorrect type!");
+  LASSERT(a, a->count == 1,
+	  "Function 'eval' passed too many arguments. "
+	  "Got %i, Expected %i", a->count, 1);
+  LASSERT(a, a->cell[0]->type == LVAL_QEXPR,
+	  "Function 'eval' passed incorrect type for argument 0. "
+	  "Got %s, Expected %s.",
+	  ltype_name(a->cell[0]->type), ltype_name(LVAL_QEXPR));
 
   lval* v = lval_take(a, 0);
   v->type = LVAL_SEXPR;
@@ -364,7 +406,10 @@ lval* builtin_eval(lenv* e, lval* a) {
 
 lval* builtin_join(lenv* e, lval* a) {
   for (int i=0; i < a->count; i++) {
-    LASSERT(a, a->cell[i]->type == LVAL_QEXPR, "Function 'join' passed incorrect type!");
+    LASSERT(a, a->cell[i]->type == LVAL_QEXPR,
+	    "Function 'join' passed incorrect type for argument %i. "
+	    "Got %s, Expected %s.",
+	    i, ltype_name(a->cell[0]->type), ltype_name(LVAL_QEXPR));
   }
 
   lval* v = lval_pop(a, 0);
