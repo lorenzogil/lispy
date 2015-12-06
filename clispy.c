f2a10670b99d91d27f4f8c7af2c51cb5aad5fc67
Lorenzo Gil Sanchez <lorenzo.gil.sanchez@gmail.com>
2015-12-06 04:29:02 +0000

    Add support for comments

--- clispy.c
+++ clispy.c
@@ -284,6 +284,10 @@ lval* lval_read(mpc_ast_t* t) {
       continue;
     }
 
+    if (strstr(t->children[i]->tag, "comment")) {
+      continue;
+    }
+
     if (strcmp(t->children[i]->tag, "regex") == 0) {
       continue;
     }
@@ -951,6 +955,7 @@ int main (int argc, char** argv) {
   mpc_parser_t* Number = mpc_new("number");
   mpc_parser_t* Symbol = mpc_new("symbol");
   mpc_parser_t* String = mpc_new("string");
+  mpc_parser_t* Comment = mpc_new("comment");
   mpc_parser_t* Sexpr = mpc_new("sexpr");
   mpc_parser_t* Qexpr = mpc_new("qexpr");
   mpc_parser_t* Expr = mpc_new("expr");
@@ -958,15 +963,16 @@ int main (int argc, char** argv) {
 
   /* Define the parsers with a language */
   mpca_lang(MPCA_LANG_DEFAULT,
-    "                                                    \
-      number : /-?[0-9]+/ ;                              \
-      symbol : /[a-zA-Z0-9_+\\-*\\/\\\\=<>!&]+/ ;        \
-      string : /\"(\\\\.|[^\"])*\"/ ;			 \
-      sexpr  : '(' <expr>* ')' ;                         \
-      qexpr  : '{' <expr>* '}' ;                         \
-      expr   : <number> | <symbol> | <string> | <sexpr> | <qexpr> ; \
-      lispy  : /^/ <expr>* /$/ ;                         \
-    ", Number, Symbol, String, Sexpr, Qexpr, Expr, Lispy);
+    "                                                                           \
+      number : /-?[0-9]+/ ;                                                     \
+      symbol : /[a-zA-Z0-9_+\\-*\\/\\\\=<>!&]+/ ;                               \
+      string : /\"(\\\\.|[^\"])*\"/ ;			                        \
+      comment : /;[^\\r\\n]*/ ;                                                 \
+      sexpr  : '(' <expr>* ')' ;                                                \
+      qexpr  : '{' <expr>* '}' ;                                                \
+      expr   : <number> | <symbol> | <string> | <comment> | <sexpr> | <qexpr> ; \
+      lispy  : /^/ <expr>* /$/ ;                                                \
+    ", Number, Symbol, String, Comment, Sexpr, Qexpr, Expr, Lispy);
 
   puts("Lispy Version 0.0.0.0.10");
   puts("Press Ctrl+c to Exit\n");
@@ -997,7 +1003,7 @@ int main (int argc, char** argv) {
 
   lenv_del(e);
 
-  mpc_cleanup(7, Number, Symbol, String, Sexpr, Qexpr, Expr, Lispy);
+  mpc_cleanup(8, Number, Symbol, String, Comment, Sexpr, Qexpr, Expr, Lispy);
 
   return 0;
 }
