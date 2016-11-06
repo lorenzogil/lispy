fc1f3e9a0e0ba3aae846ad18169c23cdba24bdb4
Lorenzo Gil Sanchez <lorenzo.gil.sanchez@gmail.com>
2016-11-06 08:25:37 -0800

    Extend the parser and add error handling

--- haskell/hklispy.hs
+++ haskell/hklispy.hs
@@ -1,70 +1,94 @@
-import qualified Text.Parsec as Parsec
+import Text.Parsec ((<|>), char, digit, eof, many1, oneOf, parse, sepBy, spaces, try, Parsec)
+import Control.Applicative ((<*))
 import System.IO
 
-data Expr = Lit Int
-          | Add [Expr]
-          | Sub [Expr]
-          | Mul [Expr]
-          | Div [Expr]
-          deriving Show
+data Symbol = Add | Sub | Mul | Div deriving Show
 
-basicNumber :: Parsec.Parsec String () Expr
+data Expr = Lit Int | Symbol Symbol | LVal [Expr] | Error String deriving Show
+
+basicNumber :: Parsec String () Expr
 basicNumber = do
-  digits <- Parsec.many1 Parsec.digit
+  digits <- many1 digit
   return (Lit (read digits :: Int))
 
-negativeNumber :: Parsec.Parsec String () Expr
+negativeNumber :: Parsec String () Expr
 negativeNumber = do
-  _ <- Parsec.char '-'
-  digits <- Parsec.many1 Parsec.digit
+  _ <- char '-'
+  digits <- many1 digit
   return (Lit ((-1) * (read digits :: Int)))
 
-numberExpr :: Parsec.Parsec String () Expr
-numberExpr = basicNumber Parsec.<|> negativeNumber
+number :: Parsec String () Expr
+number = basicNumber <|> negativeNumber
 
-opExpr :: Parsec.Parsec String () Expr
-opExpr = do
-  op <- Parsec.oneOf "+-*/"
-  _ <- Parsec.spaces
-  exprList <- Parsec.sepBy1 expr Parsec.spaces
-  return (case op of '+' -> Add exprList
-                     '-' -> Sub exprList
-                     '*' -> Mul exprList
-                     '/' -> Div exprList)
+symbol:: Parsec String () Expr
+symbol = do
+  op <- oneOf "+-*/"
+  return (case op of '+' -> Symbol Add
+                     '-' -> Symbol Sub
+                     '*' -> Symbol Mul
+                     '/' -> Symbol Div
+                     _ -> Error "Bad symbol")
 
-parensExpr :: Parsec.Parsec String () Expr
-parensExpr = do
-  _ <- Parsec.char '('
-  result <- opExpr
-  _ <- Parsec.char ')'
-  return result
+sexpr :: Parsec String () Expr
+sexpr = do
+  _ <- char '('
+  result <- sepBy expr spaces
+  _ <- char ')'
+  return (LVal result)
 
-expr :: Parsec.Parsec String () Expr
-expr =  numberExpr Parsec.<|> parensExpr
+expr :: Parsec String () Expr
+expr = try number <|> symbol <|> sexpr
 
-program :: Parsec.Parsec String () Expr
-program = opExpr
+program :: Parsec String () Expr
+program = do
+  result <- (sepBy expr spaces) <* eof
+  return (LVal result)
 
-parse :: String -> Maybe Expr
-parse text = case Parsec.parse program "(source)" text of
-                  Left _ -> Nothing
-                  Right e -> Just e
+sumExpr :: Expr -> Expr -> Expr
+sumExpr (Error e) _ = Error e
+sumExpr (Lit a) (Lit b) = Lit (a + b)
+sumExpr (Lit a) (LVal x) = sumExpr (Lit a) (eval (LVal x))
+sumExpr _ _ = Error "invalid arguments to Add"
 
-eval :: Expr -> Double
-eval ast = case ast of
-  Lit n -> fromIntegral n
-  Add exprList -> foldl (\acc i -> acc + eval i) 0 exprList
-  Sub exprList -> foldl (\acc i -> acc - eval i) (eval (head exprList)) (tail exprList)
-  Mul exprList -> foldl (\acc i -> acc * eval i) 1 exprList
-  Div exprList -> foldl (\acc i -> acc / eval i) (eval (head exprList)) (tail exprList)
+subExpr :: Expr -> Expr -> Expr
+subExpr (Error e) _ = Error e
+subExpr (Lit a) (Lit b) = Lit (a - b)
+subExpr (Lit a) (LVal x) = subExpr (Lit a) (eval (LVal x))
+subExpr _ _ = Error "invalid arguments to Sub"
+
+mulExpr :: Expr -> Expr -> Expr
+mulExpr (Error e) _ = Error e
+mulExpr (Lit a) (Lit b) = Lit (a * b)
+mulExpr (Lit a) (LVal x) = mulExpr (Lit a) (eval (LVal x))
+mulExpr _ _ = Error "invalid arguments to Mul"
+
+divExpr :: Expr -> Expr -> Expr
+divExpr (Error e) _ = Error e
+divExpr (Lit a) (Lit b) = Lit (a `div` b)
+divExpr (Lit a) (LVal x) = divExpr (Lit a) (eval (LVal x))
+divExpr _ _ = Error "invalid arguments to Div"
+
+eval :: Expr -> Expr
+eval (Lit a) = Lit a
+eval (Error e) = Error e
+eval (Symbol s) = Error ("no arguments for symbol " ++ show s)
+eval (LVal []) = Error "empty lval"
+eval (LVal [x]) = eval x
+eval (LVal (Symbol s:exprs)) = case s of
+  Add -> foldl sumExpr (Lit 0) exprs
+  Sub -> foldl subExpr (head exprs) (tail exprs)
+  Mul -> foldl mulExpr (Lit 1) exprs
+  Div -> foldl divExpr (head exprs) (tail exprs)
+eval (LVal (Error e:_)) = Error e
+eval _ = Error "invalid expression"
 
 main :: IO ()
 main = do
   putStr "lispy> "
   hFlush stdout
   input <- getLine
-  putStrLn (case parse input of
-                 Nothing -> "Error parsing input"
-                 Just ast -> show (eval ast))
+  putStrLn (case parse program "(source)" input of
+                 Left _ -> "Error parsing input"
+                 Right ast -> show (eval ast))
   main
 
