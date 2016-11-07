64d7249f4f954f19d960855f2fd53ec0e123c14c
Lorenzo Gil Sanchez <lorenzo.gil.sanchez@gmail.com>
2016-11-06 21:42:56 -0800

    Support QExpressions

--- haskell/hklispy.hs
+++ haskell/hklispy.hs
@@ -4,7 +4,11 @@ import System.IO
 
 data Symbol = Add | Sub | Mul | Div deriving Show
 
-data Expr = Lit Int | Symbol Symbol | LVal [Expr] | Error String deriving Show
+data Expr = Lit Int |
+            Symbol Symbol |
+            SVal [Expr] |
+            QVal [Expr] |
+            Error String deriving Show
 
 basicNumber :: Parsec String () Expr
 basicNumber = do
@@ -34,15 +38,22 @@ sexpr = do
   _ <- char '('
   result <- sepBy expr spaces
   _ <- char ')'
-  return (LVal result)
+  return (SVal result)
+
+qexpr :: Parsec String () Expr
+qexpr = do
+  _ <- char '{'
+  result <- sepBy expr spaces
+  _ <- char '}'
+  return (QVal result)
 
 expr :: Parsec String () Expr
-expr = try number <|> symbol <|> sexpr
+expr = try number <|> symbol <|> sexpr <|> qexpr
 
 program :: Parsec String () Expr
 program = do
   result <- (sepBy expr spaces) <* eof
-  return (LVal result)
+  return (SVal result)
 
 evalSum :: Int -> Int -> Expr
 evalSum a b = Lit (a + b)
@@ -61,27 +72,34 @@ evalBinOp :: (Int -> Int -> Expr) -> Expr -> Expr -> Expr
 evalBinOp _ (Error e) _ = Error e
 evalBinOp _ _ (Error e) = Error e
 evalBinOp f (Lit a) (Lit b) = f a b
-evalBinOp f (Lit a) (LVal x) = evalBinOp f (Lit a) (eval (LVal x))
+evalBinOp f (Lit a) (SVal x) = evalBinOp f (Lit a) (eval (SVal x))
 evalBinOp _ _ _ = Error "invalid arguments to binary operator"
 
 eval :: Expr -> Expr
 eval (Lit a) = Lit a
 eval (Error e) = Error e
 eval (Symbol s) = Error ("no arguments for symbol " ++ show s)
-eval (LVal []) = Error "empty lval"
-eval (LVal [x]) = eval x
-eval (LVal (Symbol s:exprs)) = case s of
+eval (SVal []) = Error "empty lval"
+eval (SVal [x]) = eval x
+eval (SVal (Symbol s:exprs)) = case s of
   Add -> foldl (evalBinOp evalSum) (Lit 0) exprs
   Sub -> foldl (evalBinOp evalSub) (head exprs) (tail exprs)
   Mul -> foldl (evalBinOp evalMul) (Lit 1) exprs
   Div -> foldl (evalBinOp evalDiv) (head exprs) (tail exprs)
-eval (LVal (Error e:_)) = Error e
+eval (SVal (Error e:_)) = Error e
+eval (QVal x) = QVal x
 eval _ = Error "invalid expression"
 
 printExpr :: Expr -> String
 printExpr (Lit a) = show a
+printExpr (Symbol s) = case s of
+  Add -> "+"
+  Sub -> "-"
+  Mul -> "*"
+  Div -> "/"
+printExpr (SVal x) = "(" ++ unwords (map printExpr x) ++ ")"
+printExpr (QVal x) = "{" ++ unwords (map printExpr x) ++ "}"
 printExpr (Error e) = "Error: " ++ e
-printExpr _ = "Can not print this expression"
 
 main :: IO ()
 main = do
